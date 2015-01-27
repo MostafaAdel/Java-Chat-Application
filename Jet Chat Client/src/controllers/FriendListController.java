815d4aa6d9fe6c4ff26a24506a5d4a56d7ae4c06
Mostafa_i35_EWD23 <mostafa.adel90@gmail.com>
2015-01-27 20:38:42 +0200

    Modify Server Interfaces and make all of it Extends Remote

---
+++ Jet Chat Client/src/controllers/FriendListController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ *
+ * @author Mostafa_ITI
+ */
+public class FriendListController implements FriendListInterface{
+
+    @Override
+    public void startNewChatWithFriend() {
+        
+    }
+
+    @Override
+    public void changeUserIimage() {
+            
+    }
+
+    @Override
+    public void changeUserMode() {
+            
+    }
+
+    @Override
+    public void addNewFriend() {
+            
+    }
+
+    @Override
+    public void displayNewAdvertisement() {
+            
+    }
+
+    @Override
+    public void signOut() {
+            
+    }
+
+    @Override
+    public void exit() {
+            
+    }
+
+    @Override
+    public void showFriendRequests() {
+            
+    }
+
+    @Override
+    public void deleteFriend() {
+            
+    }
+
+    @Override
+    public void searchFriend() {
+            
+    }
+
+    @Override
+    public void notifyMode() {
+            
+    }
+
+    @Override
+    public void notifyStatus() {
+            
+    }
+    
+}
