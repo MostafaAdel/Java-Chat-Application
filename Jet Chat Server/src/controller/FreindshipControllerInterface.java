8ce1614320fe8b23f1dcc0ab12a7264f60bfbbd5
Mohamed Gaber <m.gaber.ali92@gmail.com>
2015-01-28 18:04:06 +0200

    move UserProfileController class & UserProfileControllerInterface to controller package

--- Jet Chat Server/src/controller/FreindshipControllerInterface.java
+++ Jet Chat Server/src/controller/FreindshipControllerInterface.java
@@ -5,83 +5,13 @@
  */
 package controller;
 
-import entity.UserEntity;
-import java.rmi.Remote;
-import java.util.Vector;
+
 
 /**
  *
  * @author Rania
  */
-public interface FreindshipControllerInterface extends Remote{
+public interface FreindshipControllerInterface {
 
-    /**
-     * @param String userName
-     * @param String freindName
-     */
-    /**
-     * @param userName
-     *
-     * @param freindName
-     *
-     * @return boolean
-     *******************************************************
-     */
-    boolean addFreind(String userName, String freindName);
-
-    /**
-     * @param String userName
-     * @param String freindName
-     */
-    /**
-     * @return UserEntity
-     *******************************************************
-     */
-    UserEntity acceptInvitation(String userName, String freindName);
-
-    /**
-     * @param String userName
-     * @param String freindName
-     */
-    /**
-     * *********************************************************
-     */
-    boolean ignoreInvitation(String userName, String freindName);
-
-    /**
-     * @param String userName
-     * @param String freindName
-     */
-    /**
-     * *********************************************************
-     */
-    boolean rejectInvitation(String userName, String freindName);
-
-    /**
-     * @param String userName
-     * @param String freindName
-     */
-    /**
-     * *********************************************************
-     */
-    boolean deleteFreind(String userName, String freindName);
-
-    /**
-     * @param String userName
-     *
-     */
-    /**
-     * *********************************************************
-     */
-    Vector<UserEntity> getFreindList(String userName);
-
-    /**
-     * @param String userName
-     *
-     */
-    /**
-     * *********************************************************
-     */
-    Vector<UserEntity> getFreindRequest(String userName);
 
 }
